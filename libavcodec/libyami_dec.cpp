37bab73e627e8b5fc67746d3bbe7662ec72b7411
Jun Zhao <jun.zhao@intel.com>
2016-03-02 16:43:41 +0800

    Add copyright and license headers

--- libavcodec/libyami_dec.cpp
+++ libavcodec/libyami_dec.cpp
@@ -1,10 +1,26 @@
 /*
- * libyami_dec.cpp
+ * Intel Yet Another Media Infrastructure video decoder/encoder
  *
- *  Created on: 2016.2.19
- *      Author: yunzhoux
+ * Copyright (c) 2016 Intel Corporation
+ *     Zhou Yun(yunx.z.zhou@intel.com)
+ *     Jun Zhao(jun.zhao@intel.com)
+ *
+ * This file is part of FFmpeg.
+ *
+ * FFmpeg is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * FFmpeg is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with FFmpeg; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
  */
-
 
 #include "libyami.h"
 #include "libyami_utils.h"
