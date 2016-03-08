a54aa405de1d6c95702237351418b763897470d9
Yun Zhou <yunx.z.zhou@intel.com>
2016-03-08 10:04:15 +0800

    avcodec: add vp8 decoder.
    add vp8 decoder source code and configure "--enable-libyami --enable-libyami-vp8" enable "libyami_vp8" decode codec.

---
+++ libavcodec/libyami_dec_vp8.cpp
@@ -0,0 +1,75 @@
+/*
+ * Intel Yet Another Media Infrastructure video decoder/encoder
+ *
+ * Copyright (c) 2016 Intel Corporation
+ *     Zhou Yun(yunx.z.zhou@intel.com)
+ *     Jun Zhao(jun.zhao@intel.com)
+ *
+ * This file is part of FFmpeg.
+ *
+ * FFmpeg is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * FFmpeg is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with FFmpeg; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
+ */
+
+#include "libyami.h"
+
+static av_cold int yami_dec_vp8_init(AVCodecContext *avctx)
+{
+    return yami_dec_init(avctx, YAMI_MIME_VP8);
+}
+
+static int yami_dec_vp8_frame(AVCodecContext *avctx, void *data,
+                               int *got_frame, AVPacket *avpkt)
+{
+    return yami_dec_frame(avctx, data,got_frame, avpkt);
+}
+
+static av_cold int yami_dec_vp8_close(AVCodecContext *avctx)
+{
+    return yami_dec_close(avctx);
+}
+
+AVCodec ff_libyami_vp8_decoder = {
+    .name                  = "libyami_vp8",
+    .long_name             = NULL_IF_CONFIG_SMALL("libyami VP8 decoder"),
+    .type                  = AVMEDIA_TYPE_VIDEO,
+    .id                    = AV_CODEC_ID_VP8,
+    .capabilities          = CODEC_CAP_DELAY, // it is not necessary to support multi-threads
+    .supported_framerates  = NULL,
+    .pix_fmts              = (const enum AVPixelFormat[]) { AV_PIX_FMT_YAMI ,
+                                                            AV_PIX_FMT_NV12 ,
+                                                            AV_PIX_FMT_YUV420P,
+                                                            AV_PIX_FMT_NONE},
+    .supported_samplerates = NULL,
+    .sample_fmts           = NULL,
+    .channel_layouts       = NULL,
+#if FF_API_LOWRES
+    .max_lowres            = 0,
+#endif
+    .priv_class            = NULL,
+    .profiles              = NULL,
+    .priv_data_size        = sizeof(YamiDecContext),
+    .next                  = NULL,
+    .init_thread_copy      = NULL,
+    .update_thread_context = NULL,
+    .defaults              = NULL,
+    .init_static_data      = NULL,
+    .init                  = yami_dec_vp8_init,
+    .encode_sub            = NULL,
+    .encode2               = NULL,
+    .decode                = yami_dec_vp8_frame,
+    .close                 = yami_dec_vp8_close,
+    .flush                 = NULL, // TODO, add it
+    .caps_internal         = FF_CODEC_CAP_SETS_PKT_DTS
+};
