8a212efca7e8292f18ae9e9678c53796eeb44177
Jun Zhao <jun.zhao@intel.com>
2016-03-30 15:25:28 +0800

    avcodec : yamidec : fast copy based on SSE4 when read raw data from GPU to CPU.
    Remove unused code.

--- libavcodec/fast_copy.cpp
+++ libavcodec/fast_copy.cpp
@@ -1,7 +1,7 @@
 #include "fast_copy.h"
 #include <smmintrin.h>
 
-void *fast_copy( void *dst, void *src, size_t size )
+void *fast_copy(void *dst, void *src, size_t size)
 {
     char aligned;
     size_t remain;
@@ -9,15 +9,14 @@ void *fast_copy( void *dst, void *src, size_t size )
     __m128i x0, x1, x2, x3, x4, x5, x6, x7;
     __m128i *pDst, *pSrc;
 
-    if ( dst == NULL || src == NULL ) {
+    if (dst == NULL || src == NULL) {
         return NULL;
     }
 
     aligned = (((size_t) dst) | ((size_t) src)) & 0x0F;
 
-    if ( aligned != 0 ) {
-        //      printf( "Normal copy from 0x%x to 0x%x\n", src, dst );
-        //return memcpy( dst, src, size );
+    if (aligned != 0) {
+        printf("Normal copy from 0x%x to 0x%x\n", src, dst);
         return NULL;
     }
 
@@ -27,47 +26,37 @@ void *fast_copy( void *dst, void *src, size_t size )
     round = size >> 7;
     _mm_mfence();
 
-    for ( i = 0; i < round; i++ ) {
-        x0 = _mm_stream_load_si128( pSrc + 0 );
-        x1 = _mm_stream_load_si128( pSrc + 1 );
-        x2 = _mm_stream_load_si128( pSrc + 2 );
-        x3 = _mm_stream_load_si128( pSrc + 3 );
-        x4 = _mm_stream_load_si128( pSrc + 4 );
-        x5 = _mm_stream_load_si128( pSrc + 5 );
-        x6 = _mm_stream_load_si128( pSrc + 6 );
-        x7 = _mm_stream_load_si128( pSrc + 7 );
-        /*
-        __asm__ __volatile__( "movntdqa (%1), %0":"=x"(x0):"g"(pSrc) );
-        __asm__ __volatile__( "movntdqa 16(%1), %0":"=x"(x1):"g"(pSrc) );
-        __asm__ __volatile__( "movntdqa 32(%1), %0":"=x"(x2):"g"(pSrc) );
-        __asm__ __volatile__( "movntdqa 48(%1), %0":"=x"(x3):"g"(pSrc) );
-        __asm__ __volatile__( "movntdqa 64(%1), %0":"=x"(x4):"g"(pSrc) );
-        __asm__ __volatile__( "movntdqa 80(%1), %0":"=x"(x5):"g"(pSrc) );
-        __asm__ __volatile__( "movntdqa 96(%1), %0":"=x"(x6):"g"(pSrc) );
-        __asm__ __volatile__( "movntdqa 112(%1), %0":"=x"(x7):"g"(pSrc) );
-        */
-        _mm_store_si128( pDst + 0, x0 );
-        _mm_store_si128( pDst + 1, x1 );
-        _mm_store_si128( pDst + 2, x2 );
-        _mm_store_si128( pDst + 3, x3 );
-        _mm_store_si128( pDst + 4, x4 );
-        _mm_store_si128( pDst + 5, x5 );
-        _mm_store_si128( pDst + 6, x6 );
-        _mm_store_si128( pDst + 7, x7 );
+    for (i = 0; i < round; i++) {
+        x0 = _mm_stream_load_si128(pSrc + 0);
+        x1 = _mm_stream_load_si128(pSrc + 1);
+        x2 = _mm_stream_load_si128(pSrc + 2);
+        x3 = _mm_stream_load_si128(pSrc + 3);
+        x4 = _mm_stream_load_si128(pSrc + 4);
+        x5 = _mm_stream_load_si128(pSrc + 5);
+        x6 = _mm_stream_load_si128(pSrc + 6);
+        x7 = _mm_stream_load_si128(pSrc + 7);
+
+        _mm_store_si128(pDst + 0, x0);
+        _mm_store_si128(pDst + 1, x1);
+        _mm_store_si128(pDst + 2, x2);
+        _mm_store_si128(pDst + 3, x3);
+        _mm_store_si128(pDst + 4, x4);
+        _mm_store_si128(pDst + 5, x5);
+        _mm_store_si128(pDst + 6, x6);
+        _mm_store_si128(pDst + 7, x7);
         pSrc += 8;
         pDst += 8;
     }
 
-    if ( remain >= 16 ) {
+    if (remain >= 16) {
         size = remain;
         remain = size & 0xF;
         round = size >> 4;
 
-        for ( i = 0; i < round; i++ ) {
-            //__asm__ ( "movntdqa (%1), %0":"=x"(x0):"g"(pSrc) );
-            x0 = _mm_stream_load_si128( pSrc + 0 );
+        for (i = 0; i < round; i++) {
+            x0 = _mm_stream_load_si128(pSrc + 0);
             pSrc += 1;
-            _mm_store_si128( pDst, x0 );
+            _mm_store_si128(pDst, x0);
             pDst += 1;
         }
     }
@@ -76,7 +65,7 @@ void *fast_copy( void *dst, void *src, size_t size )
         char *ps = (char *)(pSrc);
         char *pd = (char *)(pDst);
 
-        for ( i = 0; i < remain; i++ ) {
+        for (i = 0; i < remain; i++) {
             pd[i] = ps[i];
         }
     }
