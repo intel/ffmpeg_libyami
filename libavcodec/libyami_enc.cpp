4a1ece107fc33f4549bbc3c78f504ce7d55620cf
Jun Zhao <jun.zhao@intel.com>
2016-05-13 13:43:41 +0800

    lavf : libyami : Using CQP as default ratecontrol setting.
        Mapping ratecontrol setting used ffmpeg option.
    
        ratecontrol method selected
            When ‘global_quality’ is specified, a quality-based mode is used.
            Specifically this means either
                - CQP - constant quantizer scale, when the ‘qscale’ codec
                    flag is also set (the ‘-qscale’ avconv option). and
    		CQP 26 as default ratecontrol setting.
            Otherwise, a bitrate-based mode is used. For all of those, you
            should specify at least the desired average bitrate with
            the ‘b’ option.
                - CBR - constant bitrate, when ‘maxrate’ is specified and
                    equal to the average bitrate.
                - VBR - variable bitrate, when ‘maxrate’ is specified, but
                    is higher than the average bitrate.

--- libavcodec/libyami_enc.cpp
+++ libavcodec/libyami_enc.cpp
@@ -261,18 +261,23 @@ static int yami_enc_init(AVCodecContext *avctx)
         encVideoParams.rcParams.initQP = av_clip(quant, 1, 52);
 
         rc_desc = "constant quantization parameter (CQP)";
-    } else if (avctx->rc_max_rate == avctx->bit_rate) {
-        encVideoParams.rcMode = RATE_CONTROL_CBR;
-        encVideoParams.rcParams.bitRate = avctx->bit_rate;
-        encVideoParams.rcParams.targetPercentage = 100;
-
-        rc_desc = "constant bitrate (CBR)";
     } else if (avctx->rc_max_rate > avctx->bit_rate) {
         encVideoParams.rcMode = RATE_CONTROL_VBR;
         encVideoParams.rcParams.bitRate = avctx->rc_max_rate;
 
         encVideoParams.rcParams.targetPercentage = (100 * avctx->bit_rate)/avctx->rc_max_rate;
         rc_desc = "variable bitrate (VBR)";
+    } else if (avctx->rc_max_rate == avctx->bit_rate) {
+        encVideoParams.rcMode = RATE_CONTROL_CBR;
+        encVideoParams.rcParams.bitRate = avctx->bit_rate;
+        encVideoParams.rcParams.targetPercentage = 100;
+
+        rc_desc = "constant bitrate (CBR)";
+    } else {
+        encVideoParams.rcMode = RATE_CONTROL_CQP;
+        encVideoParams.rcParams.initQP = 26;
+
+        rc_desc = "constant quantization parameter (CQP) as default";
     }
 
     av_log(avctx, AV_LOG_VERBOSE, "Using the %s ratecontrol method\n", rc_desc);
